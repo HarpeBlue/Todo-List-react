e9624c72e549be029a1275274ac15700d83846c1
HarpeBlue <hspe2507@gmail.com>
2022-09-10 17:18:29 -0500

    :recycle: changed useState for useReducer

--- src/hooks/useLocaleStorage.js
+++ src/hooks/useLocaleStorage.js
@@ -1,10 +1,77 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
+
+const initialState = (initialValue) => ({
+  loading: false,
+  sincronized: true,
+  item: initialValue,
+  error: false,
+  errorMessage: "",
+});
+
+const actionTypes = {
+  ERROR: "ERROR",
+  SUCCESS: "SUCCESS",
+  SAVE: "SAVE",
+  SINCRONIZE: "SINCRONIZE",
+  DEFAULT: "DEFAULT",
+};
+
+const reducer = (state, action) =>
+  ({
+    [actionTypes.ERROR]: {
+      ...state,
+      error: true,
+      errorMessage: action.payload,
+    },
+    [actionTypes.SUCCESS]: {
+      ...state,
+      item: action.payload,
+      loading: false,
+      sincronized: true,
+    },
+    [actionTypes.SAVE]: {
+      ...state,
+      item: action.payload,
+    },
+    [actionTypes.SINCRONIZE]: {
+      ...state,
+      loading: true,
+      sincronized: false,
+    },
+    [actionTypes.DEFAULT]: {
+      ...state,
+    },
+  }[action.type || actionTypes.DEFAULT]);
 
 export const useLocalStorage = (itemName, initialValue) => {
-  const [sincronized, setSincronized] = useState(true);
-  const [item, setItem] = useState(initialValue);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [{ sincronized, item, loading, error }, dispatch] = useReducer(
+    reducer,
+    initialState(initialValue)
+  );
+
+  // Action Creators
+  const onError = (error) =>
+    dispatch({
+      type: actionTypes.ERROR,
+      payload: error,
+    });
+
+  const onSuccess = (item) =>
+    dispatch({
+      type: actionTypes.SUCCESS,
+      payload: item,
+    });
+
+  const onSave = (item) =>
+    dispatch({
+      type: actionTypes.SAVE,
+      payload: item,
+    });
+
+  const onSincronize = (item) =>
+    dispatch({
+      type: actionTypes.SINCRONIZE,
+    });
 
   useEffect(() => {
     setTimeout(() => {
@@ -18,12 +85,9 @@ export const useLocalStorage = (itemName, initialValue) => {
         } else {
           parsedItem = JSON.parse(localStorageItem);
         }
-        setItem(parsedItem);
-        setLoading(false);
-        setSincronized(true);
+        onSuccess(parsedItem);
       } catch (error) {
-        setError(error);
-        console.error(error);
+        onError(error);
       }
     }, 2000);
   }, [sincronized]);
@@ -31,16 +95,11 @@ export const useLocalStorage = (itemName, initialValue) => {
   const saveItem = (newItem) => {
     try {
       localStorage.setItem(itemName, JSON.stringify(newItem));
-      setItem(newItem);
+      onSave(newItem);
     } catch (error) {
-      setError(error);
-      console.error(error);
+      onError(error);
     }
   };
 
-  const sincronizeItem = () => {
-    setLoading(true);
-    setSincronized(false);
-  };
-  return { item, saveItem, loading, error, sincronizeItem };
+  return { item, saveItem, loading, error, sincronizeItem: onSincronize };
 };
